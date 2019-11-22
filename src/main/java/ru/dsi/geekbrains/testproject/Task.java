6e63d33746f504a7d49d164ee99cbb6d2745b015
Cromeshnic <cromeshnic@gmail.com>
2019-11-22 10:37:28 +0800

    homework11 - hibernate

--- src/main/java/ru/dsi/geekbrains/testproject/Task.java
+++ src/main/java/ru/dsi/geekbrains/testproject/Task.java
@@ -1,16 +1,35 @@
 package ru.dsi.geekbrains.testproject;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.TableGenerator;
 import java.io.Serializable;
 
+@Entity
 class Task implements Serializable {
     private static final long serialVersionUID = 8213894426665978662L;
 
+    @Id
+    @GeneratedValue(generator="sqlite")
+    @TableGenerator(name="sqlite", table="sqlite_sequence",
+            pkColumnName="name", valueColumnName="seq",
+            pkColumnValue="task_id")
     private long id;
+    
     private String title;
     private String owner;
     private String assignee;
     private String description;
     private String status;
+
+    public Task(){
+        this.title="";
+        this.owner="";
+        this.assignee="";
+        this.description="";
+        this.status="";
+    }
 
     public Task(long id, String title, String owner) {
         this.id = id;
--- src/main/java/ru/dsi/geekbrains/testproject/TaskService.java
+++ src/main/java/ru/dsi/geekbrains/testproject/TaskService.java
@@ -3,23 +3,24 @@ package ru.dsi.geekbrains.testproject;
 import ru.dsi.geekbrains.testproject.exceptions.MyException;
 
 import java.io.*;
-import java.sql.SQLException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class TaskService{
+class TaskService implements AutoCloseable{
     private TaskRepository taskRepository;
 
     public TaskService() throws MyException{
         //this.taskRepository = new LinkedListTaskRepository();
-        try {
+        /*try {
             this.taskRepository = new SQLiteTaskRepository("data/tasks.db", true);
         } catch (ClassNotFoundException | SQLException e) {
             throw new MyException("Невозможно инициализировать репозиторий", e);
-        }
+        }*/
+        this.taskRepository = new HibernateTaskRepository(true);
     }
+
 
     public void addTask(Task task){
         try {
@@ -119,6 +120,58 @@ class TaskService{
     public List<Task> loadFromFile(File file) throws IOException, ClassNotFoundException {
         try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))){
             return (List<Task>) in.readObject();
+        }
+    }
+
+    /**
+     * Closes this resource, relinquishing any underlying resources.
+     * This method is invoked automatically on objects managed by the
+     * {@code try}-with-resources statement.
+     *
+     * <p>While this interface method is declared to throw {@code
+     * Exception}, implementers are <em>strongly</em> encouraged to
+     * declare concrete implementations of the {@code close} method to
+     * throw more specific exceptions, or to throw no exception at all
+     * if the close operation cannot fail.
+     *
+     * <p> Cases where the close operation may fail require careful
+     * attention by implementers. It is strongly advised to relinquish
+     * the underlying resources and to internally <em>mark</em> the
+     * resource as closed, prior to throwing the exception. The {@code
+     * close} method is unlikely to be invoked more than once and so
+     * this ensures that the resources are released in a timely manner.
+     * Furthermore it reduces problems that could arise when the resource
+     * wraps, or is wrapped, by another resource.
+     *
+     * <p><em>Implementers of this interface are also strongly advised
+     * to not have the {@code close} method throw {@link
+     * InterruptedException}.</em>
+     * <p>
+     * This exception interacts with a thread's interrupted status,
+     * and runtime misbehavior is likely to occur if an {@code
+     * InterruptedException} is {@linkplain Throwable#addSuppressed
+     * suppressed}.
+     * <p>
+     * More generally, if it would cause problems for an
+     * exception to be suppressed, the {@code AutoCloseable.close}
+     * method should not throw it.
+     *
+     * <p>Note that unlike the {@link Closeable#close close}
+     * method of {@link Closeable}, this {@code close} method
+     * is <em>not</em> required to be idempotent.  In other words,
+     * calling this {@code close} method more than once may have some
+     * visible side effect, unlike {@code Closeable.close} which is
+     * required to have no effect if called more than once.
+     * <p>
+     * However, implementers of this interface are strongly encouraged
+     * to make their {@code close} methods idempotent.
+     *
+     * @throws Exception if this resource cannot be closed
+     */
+    @Override
+    public void close() throws Exception {
+        if(taskRepository instanceof AutoCloseable){
+            ((AutoCloseable) taskRepository).close();
         }
     }
 }
